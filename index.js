279c04de9ba5475383ae7082dddaad7b23ebad3c
Taranpreetkaur29 <82471766+Taranpreetkaur29@users.noreply.github.com>
2021-06-01 13:22:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const secondhand=document.querySelector(".seconds")
+const minutehand=document.querySelector(".minutes")
+const hourhand=document.querySelector(".hour")
+
+function date() {
+    const newdate=new Date();
+
+    const sec=newdate.getSeconds()
+    const seccal=((sec/60)*360)+90;
+    secondhand.style.transform = `rotate(${seccal}deg)`;
+
+    const min=newdate.getMinutes()
+    const mincal=((min/60)*360)+ ((sec/60)*6) +90;
+    minutehand.style.transform = `rotate(${mincal}deg)`;
+
+    const hr=newdate.getHours()
+    const hrcal=((hr/12)*360)+ ((mins/60)*30)+90;
+    hourhand.style.transform = `rotate(${hrcal}deg)`;
+}
+
+setInterval(date,1000);
